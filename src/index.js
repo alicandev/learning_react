ea465f790aaf430bec7e1154cd2d646f146840ef
Alican Demirtas <alican@compositional-it.com>
2020-05-03 07:20:14 +0100

    Created some function components.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 
-class Message extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1 style={{color: this.props.color}}>{ this.props.msg }</h1>
-                <p>I'll check back in {this.props.minutes} minutes.</p>
-            </div>
-        )
-    }
+const getPercent = decimal => decimal * 100 + "%";
+
+const calculateGoalProgress = (total, goal) => getPercent(total/goal);
+
+const SkiDayCounter = ({ total, powder, backcountry, goal }) => 
+    <section>
+        <div>
+            <p>Total Days: { total }</p>
+        </div>
+        <div>
+            <p>Powder Days: { powder }</p>
+        </div>
+        <div>
+            <p>Backcountry Days: { backcountry }</p>
+        </div>
+        <div>
+            <p>Goal Progress: { calculateGoalProgress(total, goal) }</p>
+        </div>
+    </section>
+
+let skiData = {
+    total: 50,
+    powder: 20,
+    backcountry: 10,
+    goal: 100
 }
 
-let style = {
-  backgroundColor: 'orange',
-  color: 'white',
-  fontFamily: 'Arial'
-}
-
-ReactDOM.render(<Message color="blue" msg="How are you?" minutes={20}/>, document.getElementById('root')
+render(
+    <SkiDayCounter
+        total = { skiData.total }
+        powder = { skiData.powder }
+        backcountry = { skiData.backcountry }
+        goal = { skiData.goal }
+    />, 
+    document.getElementById('root')
 )
