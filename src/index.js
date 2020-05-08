e85fb6d2b0e3eb30b5601bb8e2afadb4d693db7d
Alican Demirtas <alican@compositional-it.com>
2020-05-08 11:31:27 +0100

    Composing stateful components.

--- src/index.js
+++ src/index.js
@@ -1,39 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 
-const getPercent = decimal => decimal * 100 + "%";
+const ROOT = document.getElementById('root');
 
-const calculateGoalProgress = (total, goal) => getPercent(total/goal);
-
-const SkiDayCounter = ({ total, powder, backcountry, goal }) => 
+let Book = ({ title, author, pages }) =>
     <section>
-        <div>
-            <p>Total Days: { total }</p>
-        </div>
-        <div>
-            <p>Powder Days: { powder }</p>
-        </div>
-        <div>
-            <p>Backcountry Days: { backcountry }</p>
-        </div>
-        <div>
-            <p>Goal Progress: { calculateGoalProgress(total, goal) }</p>
-        </div>
+        <h2>{ title }</h2>
+        <p>by: { author }</p>
+        <p>Pages: { pages } pages</p>
     </section>
 
-let skiData = {
-    total: 50,
-    powder: 20,
-    backcountry: 10,
-    goal: 100
+class Library extends React.Component {
+    state = { 
+        open: true,
+        freeBookmark: true 
+    }
+    
+    toggleOpenClosed = () => 
+        this.setState(prevState => ({ open: !prevState.open }))
+    
+    render() {
+        console.log(this.state);
+        let { books } = this.props;
+        return (
+            <div>
+                <h1>The library is { this.state.open ? "open" : "closed" }.</h1>
+                <button onClick={ this.toggleOpenClosed }>Change</button>
+                { books.map(
+                    (book, i) =>
+                        <Book
+                            key={ i }
+                            title={ book.title }
+                            author={ book.author }
+                            pages={ book.pages }
+                        />
+                ) }
+            </div>
+        );
+    }
 }
 
-render(
-    <SkiDayCounter
-        total = { skiData.total }
-        powder = { skiData.powder }
-        backcountry = { skiData.backcountry }
-        goal = { skiData.goal }
-    />, 
-    document.getElementById('root')
-)
+let bookList = [
+    { "title": "The Sun Also Rises", "author": "Ernest Hemingway", "pages": 260 },
+    { "title": "Second Book Woah", "author": "Someone Important", "pages": 123124 },
+    { "title": "Third or Fourth IDK", "author": "Someone Who Knows", "pages": 3333 }
+]
+
+let body = <Library books={ bookList }/>
+
+render(body, ROOT);
